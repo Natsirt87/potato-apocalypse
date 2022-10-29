a2ee6e0b0d287d52c3cee0ef04d55babad1a223f
Tristan <tristancommons@outlook.com>
2022-10-29 13:41:23 -0700

    Implemented PID-based player rotation to mouse.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,16 @@ public class PlayerMovement : MonoBehaviour
     public float pGain = 1f;
     public float dGain = 1f;
     public float iGain = 1f;
+    public float iSaturation = 1f;
+    public float rotatePower = 1f;
 
     private Camera _camera;
     private Rigidbody2D _body;
-    
+
+    private bool _derivativeInit = false;
+    private float _valueLast = 0f;
+    private float _integrationStored = 0f;
+
     void Start()
     {
         _camera = Camera.main;
@@ -22,59 +28,40 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        RotateToMouse();
-    }
-    // Rotate the player to look at the mouse position with physics magic 
-    private void RotateToMouse()
-    {
-        float current = _body.rotation;
+        
+        float currentRotation = _body.rotation;
+        Vector3 vectorToMouse = _camera.ScreenToWorldPoint(Input.mousePosition) - (Vector3)_body.position;
+        float targetRotation = Quaternion.LookRotation(Vector3.forward, vectorToMouse).eulerAngles.z;
         float dt = Time.fixedDeltaTime;
         
-        // Get angle to mouse position
-        Vector3 vectorToMouse = _camera.ScreenToWorldPoint(Input.mousePosition) - (Vector3)_body.position;
-        float target = Quaternion.LookRotation(Vector3.forward, vectorToMouse).eulerAngles.z;
-
+        _body.AddTorque(PidRotation(currentRotation, targetRotation, dt ) * rotatePower, ForceMode2D.Force);
+    }
+    
+    // Rotate the player to look at the mouse position with physics magic 
+    private float PidRotation(float current, float target, float dt)
+    {
         float error = AngleDifference(target, current);
-
         float p = pGain * error;
-        
 
+        float valueRateOfChange = AngleDifference(current, _valueLast);
+        _valueLast = current;
 
+        float d = 0f;
+        if (_derivativeInit)
+        {
 
-        // bool shouldDecelerate;
-        // bool shouldAccelerate;
-        // if (turnAngle < 0)
-        // {
-        //     
-        //     shouldDecelerate = angVel * stopRotateTime <= turnAngle;
-        //     shouldAccelerate = angVel > -rotateSpeed;
-        // }
-        // else
-        // {
-        //     shouldDecelerate = _body.angularVelocity * stopRotateTime >= turnAngle;
-        //     shouldAccelerate = angVel < rotateSpeed;
-        // }
-        //
-        // // We're going to overshoot (we've reached the time when we should decelerate)
-        // if (shouldDecelerate)
-        // {
-        //     Debug.Log("Deceleration time!");
-        //     
-        //     // Calculate angle needed to slow down by each frame in order to stop rotating in stopRotateTime
-        //     //float decelForce = (-turnAngle / rotateDecelScale) * _body.inertia;
-        //     
-        //     // Apply torque to decelerate
-        //     _body.AddTorque(decelForce, ForceMode2D.Force);
-        // }
-        // // It's time to accelerate
-        // else if (Mathf.Abs(turnAngle) > 0.1f && shouldAccelerate)
-        // {
-        //     // Accelerate to rotate towards mouse
-        //     Debug.Log("Acceleration time!");
-        //
-        //     // Apply torque to accelerate
-        //     _body.AddTorque(turnAngle > 0 ? rotateAccelForce : -rotateAccelForce, ForceMode2D.Force);
-        // }
+            d = dGain * -valueRateOfChange;
+        }
+        else
+        {
+            _derivativeInit = true;
+        }
+
+        _integrationStored += (error * dt);
+        _integrationStored = Mathf.Clamp(_integrationStored + (error * dt), -iSaturation, iSaturation);
+        float i = iGain * _integrationStored;
+
+        return p + i + d;
     }
 
     private float AngleDifference(float a, float b)
