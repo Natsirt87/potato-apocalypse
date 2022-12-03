272c995d206d8a10256486a99b8fcaf7a16eb291
bykovav8 <nikaby879@gmail.com>
2022-12-03 15:16:52 -0800

    Added slicer and initial grenade shooting

--- Assets/Scripts/Weaponry/Slicer.cs
+++ Assets/Scripts/Weaponry/Slicer.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 
 public class Slicer : Weapon
 {
-    // public float projectileForce = 10f;
-    // public float shootInterval = .2f;
-    // public float spawnOffset = 1f;
+    // implement grenade launcher style weapon
+    // circular projectiles that move slowly and explode on impact dealing damage in a radius.
+    public float projectileForce = 10f;
+    public float shootInterval = .2f;
+    public float spawnOffset = 1f;
 
+    //spawning laser from knife 
+    protected override IEnumerator Shoot()
+    {
+        canShoot = false;
+        GameObject spawnedLaser = Instantiate(projectile);
+
+        spawnedLaser.transform.right = this.transform.parent.up;
+        spawnedLaser.transform.position = this.transform.position + (transform.parent.up * spawnOffset);
+
+        Rigidbody2D laserRB = spawnedLaser.GetComponent<Rigidbody2D>();
+
+        Vector3 force = transform.parent.up * projectileForce;
+        laserRB.AddForce(force, ForceMode2D.Impulse);
+
+        // wait for .4 seconds    
+        yield return new WaitForSeconds(shootInterval);
+        canShoot = true;
+    }
 
 }
